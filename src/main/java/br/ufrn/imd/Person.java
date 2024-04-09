5c86266d0d92cae4e56b59c837546193ba4e5aa3
iadsonv1n1 <i.vini@hotmail.com>
2024-04-09 12:12:10 -0300

    Teste 1

---
+++ src/main/java/br/ufrn/imd/Person.java
@@ -0,0 +1,16 @@
+package br.ufrn.imd;
+
+public class Person {
+    public String name;
+    public String hometown;
+
+    Person() { }
+
+    public String name() {
+        return name;
+    }
+
+    public String hometown() {
+        return hometown;
+    }
+}
